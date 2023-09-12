83b578d466e2aabab16ca85c44cf06ecee1fa3bc
gasangw <thomasgasangwa@gmail.com>
2023-09-12 08:48:04 +0200

    Add pictures and the nav folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import Nav from "./Components/Nav";
 function App() {
   return (
     <div className="App">
+      <Nav />
         <h1 className="text-2xl text-green-700 text-center">Hello</h1>
     </div>
   );
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='text-3xl text-green-600'>Nav</div>
+  )
+}
+
+export default Nav
