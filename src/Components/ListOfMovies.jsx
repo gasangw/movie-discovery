7694d93064f2eb9b28e565c0b32b1672ee8dd328
gasangw <thomasgasangwa@gmail.com>
2023-09-12 18:01:59 +0200

    add the search section

--- src/Components/ListOfMovies.jsx
+++ src/Components/ListOfMovies.jsx
@@ -3,17 +3,12 @@ import React from 'react'
 function ListOfMovies(props) {
     const { backdrop_path,
         release_date,title} = props.data
-       // {backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie.backdrop_path}')`}
   return (
-    <div className='container'>
-        <div>
-           <div className='grid text-left card' data-testid="movie-card">
-              <img src={`'https://image.tmdb.org/t/p/original${backdrop_path}`} alt={title} data-testid="movie-poster"/>
+           <div className='grid text-left' data-testid="movie-card">
+              <img src={`https://image.tmdb.org/t/p/w500/${backdrop_path}`} alt={title} data-testid="movie-poster"/>
               <h2 data-testid="movie-title">{title}</h2>
               <p data-testid="movie-release-date">{release_date}</p>
            </div>
-        </div>
-    </div>
   )
 }
 
--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -3,6 +3,7 @@ import ListOfMovies from './ListOfMovies';
 
 function Movies() {
   const [movies, setMovies] = useState([])
+  const [search, setSearch] = useState('')
     useEffect(() => {
       const fetchMovies = async () => {
         const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=f12569fc3f929239fab0c4a8fa065dda')
@@ -11,15 +12,21 @@ function Movies() {
       }
       fetchMovies();
     }, [])
+
+    const handleSearch = (e) => {
+       setSearch(e.target.value)
+    }
   return (
     <div>
+      <input type="text" placeholder='What movie do you want to watch?' className='flex mx-auto' onChange={handleSearch}/>
        {
-        movies.map((movie)=> {
-            console.log(movie)
+        movies && movies.filter(values => values.title.includes(search)).map((movie)=> {
            return (
-            <div key={movie.id}>
-               <ListOfMovies  data={movie}/>
-            </div>
+             <div className='container flex flex-row border-2 border-green-600'>
+                <div key={movie.id} className='flex flex-row'>
+                   <ListOfMovies  data={movie}/>
+                </div>
+             </div>
            )
         })
        }
