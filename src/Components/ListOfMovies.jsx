c7b71ea96ea5e3914fd2ee0185a0dac3984846ec
gasangw <thomasgasangwa@gmail.com>
2023-09-12 16:41:41 +0200

    Implement the card design

--- src/Components/ListOfMovies.jsx
+++ src/Components/ListOfMovies.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 function ListOfMovies(props) {
-    const [poster_path,vote_average, release_date,] = props.data
+    const [poster_path,vote_average, release_date,title] = props.data
   return (
-    <div>
-
+    <div className='container'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
+           <div className='grid text-left rounded-lg'>
+              <img src="" alt="" />
+           </div>
+        </div>
     </div>
   )
 }
