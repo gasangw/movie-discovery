37a538f1fe20f7d8bdd783fd1f7b16f3e9ff25bd
gasangw <thomasgasangwa@gmail.com>
2023-09-12 19:59:06 +0200

    Change the format of the code

--- src/Components/ListOfMovies.jsx
+++ src/Components/ListOfMovies.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function ListOfMovies(props) {
-    const { poster_path,
-        release_date,title} = props.data
+  const { poster_path, release_date, title } = props.data;
   return (
-           <div className='grid text-left card' data-testid="movie-card">
-              <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title} data-testid="movie-poster"/>
-              <h2 data-testid="movie-title" className='text-base font-sans font-bold'>{title}</h2>
-              <p data-testid="movie-release-date">{release_date}</p>
-           </div>
-  )
+    <div className="grid text-left card" data-testid="movie-card">
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+        alt={title}
+        data-testid="movie-poster"
+      />
+      <h2 data-testid="movie-title" className="text-base font-sans font-bold">
+        {title}
+      </h2>
+      <p data-testid="movie-release-date">{release_date}</p>
+    </div>
+  );
 }
 
-export default ListOfMovies
+export default ListOfMovies;
--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,37 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import ListOfMovies from './ListOfMovies';
+import React, { useState, useEffect } from "react";
+import ListOfMovies from "./ListOfMovies";
 
 function Movies() {
-  const [movies, setMovies] = useState([])
-  const [search, setSearch] = useState('')
-    useEffect(() => {
-      const fetchMovies = async () => {
-        const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=f12569fc3f929239fab0c4a8fa065dda')
-        const data = await response.json();
-        setMovies([...data.results])
-      }
-      fetchMovies();
-    }, [])
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/top_rated?api_key=f12569fc3f929239fab0c4a8fa065dda"
+      );
+      const data = await response.json();
+      setMovies([...data.results]);
+    };
+    fetchMovies();
+  }, []);
 
-    const handleSearch = (e) => {
-       setSearch(e.target.value)
-    }
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
   return (
     <div>
-      <input type="text" placeholder='What movie do you want to watch?' className='flex mx-auto' onChange={handleSearch}/>
-    <div className='container grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-4'>
-       {
-        movies && movies.filter(values => values.title.toLowerCase().includes(search.toLowerCase())).map((movie)=> {
-           return (
-                <div key={movie.id} className='flex flex-row'>
-                   <ListOfMovies  data={movie}/>
+      <input
+        type="text"
+        placeholder="What movie do you want to watch?"
+        className="flex mx-auto"
+        onChange={handleSearch}
+      />
+      <div className="container grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-4">
+        {movies &&
+          movies
+            .filter((values) =>
+              values.title.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((movie) => {
+              return (
+                <div key={movie.id} className="flex flex-row">
+                  <ListOfMovies data={movie} />
                 </div>
-           )
-        })
-       }
+              );
+            })}
       </div>
     </div>
-  )
+  );
 }
 
-export default Movies
+export default Movies;
