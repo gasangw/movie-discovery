7a8d2fda3007c234d2a4d1c4104241d3ef3e06db
gasangw <thomasgasangwa@gmail.com>
2023-09-13 22:34:12 +0200

    Change the structure of the details page

--- src/Components/MovieDetails.jsx
+++ src/Components/MovieDetails.jsx
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom'
 function MovieDetails({movies}) {
   const Id = useParams()
   const moveClickedOn = movies.find((movie) => movie.id+'' === Id.id)
-  const {title, overview, popularity, release_date} = moveClickedOn
+  const {title,poster_path, overview, popularity, release_date} = moveClickedOn
   const date = new Date(release_date).toUTCString()
   return (
     <div className='container py-10'>
@@ -16,12 +16,22 @@ function MovieDetails({movies}) {
             </svg>
           </Link> 
         </div>
-        <div className='text-left bg-zinc-300 py-10 mt-8 px-4 space-y-3'>
-            <h1 className='text-lg font-medium' data-testid="movie-title"><span className='font-bold'>Title:  </span>{title}</h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 bg-zinc-300 py-10 mt-8 px-4 gap-5'>
+           <div>
+           <img
+              src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+              alt={title}
+              data-testid="movie-poster"
+              className='w-full h-96'
+            />
+           </div>
+           <div>
+           <h1 className='text-lg font-medium' data-testid="movie-title"><span className='font-bold'>Title:  </span>{title}</h1>
             <h3 className='text-base' data-testid="movie-overview"><span className='font-bold'>Overview:  </span>{overview}</h3>
             <p className='text-base' data-testid="movie-runtime"><span className='font-bold'>Runtime:  </span>{popularity}</p>
             <p className='italic font-normal' data-testid="movie-release-date"><span className='font-bold'>Release Date:  </span>{date}</p>
         </div>
+           </div>
     </div>
   )
 }
