7f83df839eb4c90f9732d718edf8ed3ac8979c11
gasangw <thomasgasangwa@gmail.com>
2023-09-13 15:57:07 +0200

    Update the main page section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import instagram from '../Images/fa-brands_instagram.png'
-import twitter from '../Images/fa-brands_twitter.png'
-import youtube from '../Images/fa-brands_youtube.png'
-import facebook from '../Images/Vector.png'
+import React from "react";
+import instagram from "../Images/fa-brands_instagram.png";
+import twitter from "../Images/fa-brands_twitter.png";
+import youtube from "../Images/fa-brands_youtube.png";
+import facebook from "../Images/Vector.png";
 
 function Footer() {
   return (
-    <div className='container flex flex-col justify-center items-center space-y-3 py-28'>
-        <ul className='flex space-x-5'>
-        <li><img src={facebook} alt="facebook" /></li>
-        <li><img src={instagram} alt="instagram" /></li>
-        <li><img src={twitter} alt="twitter" /></li>
-        <li><img src={youtube} alt="youtube" /></li>
-       </ul>
-       <ul className='flex space-x-3 font-bold'>
-        <li className='hover:underline'>Conditions of Use</li>
-        <li className='hover:underline'>Privacy & Policy</li>
-        <li className='hover:underline'>Press Room</li>
-       </ul>
-       <p className='text-base text-[#6B7280]'>© 2021 MovieBox by Adriana Eka Prayudha</p>
+    <div className="container flex flex-col justify-center items-center space-y-3 py-24">
+      <ul className="flex space-x-5">
+        <li>
+          <img src={facebook} alt="facebook" />
+        </li>
+        <li>
+          <img src={instagram} alt="instagram" />
+        </li>
+        <li>
+          <img src={twitter} alt="twitter" />
+        </li>
+        <li>
+          <img src={youtube} alt="youtube" />
+        </li>
+      </ul>
+      <ul className="flex space-x-3 font-bold">
+        <li className="hover:underline">Conditions of Use</li>
+        <li className="hover:underline">Privacy & Policy</li>
+        <li className="hover:underline">Press Room</li>
+      </ul>
+      <p className="text-base text-[#6B7280]">
+        © 2021 MovieBox by Adriana Eka Prayudha
+      </p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
